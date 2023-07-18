960bb0006d5f268ea0eb70ab13b711f0ed8df43a
vytskalt <VytskaLT@protonmail.com>
2023-07-18 13:28:04 +0300

    add uri constructor

--- src/main/kotlin/net/megavex/redikt/RedisEndpoint.kt
+++ src/main/kotlin/net/megavex/redikt/RedisEndpoint.kt
@@ -1,8 +1,14 @@
 package net.megavex.redikt
 
+import java.net.URI
+
 public data class RedisEndpoint(val host: String, val port: Int = DEFAULT_PORT) {
     public companion object {
         public const val DEFAULT_PORT: Int = 6379
+    }
+
+    public constructor(uri: URI) : this(uri.host, uri.port) {
+        require(uri.scheme == "redis") { "invalid scheme ${uri.scheme}" }
     }
 
     init {
