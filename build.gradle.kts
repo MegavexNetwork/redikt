b921d324cbaab08c55f1fadb654dc21ad39c8562
vytskalt <VytskaLT@protonmail.com>
2023-07-29 15:56:53 +0300

    integration test the client

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     `maven-publish`
+    `jvm-test-suite`
 }
 
 group = "net.megavex"
@@ -15,8 +16,32 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+@Suppress("UnstableApiUsage")
+testing {
+    suites {
+        register<JvmTestSuite>("integrationTest") {
+            dependencies {
+                implementation(project())
+            }
+        }
+    }
+}
+
+@Suppress("UnstableApiUsage")
+tasks.named("check") {
+    dependsOn(testing.suites.named("integrationTest"))
+}
+
+val integrationTestImplementation: Configuration by configurations.getting {
+    extendsFrom(configurations.testImplementation.get())
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.getByName<Test>("integrationTest") {
+    project.findProperty("redis.uri")?.let { systemProperty("redis.uri", it) }
 }
 
 kotlin {
---
+++ src/integrationTest/kotlin/net/megavex/redikt/client/RedisClientTest.kt
@@ -0,0 +1,25 @@
+package net.megavex.redikt.client
+
+import io.ktor.network.selector.SelectorManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import net.megavex.redikt.RedisEndpoint
+import net.megavex.redikt.command.commands.ping
+import org.junit.jupiter.api.Assumptions.assumeTrue
+import java.net.URI
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class RedisClientTest {
+    @Test
+    fun connectionTest() = runBlocking {
+        val uri = System.getProperty("redis.uri")
+        assumeTrue(uri != null, "redis.uri system property not set")
+
+        val selectorManager = SelectorManager(Dispatchers.IO)
+        val client = RedisClient(selectorManager, RedisEndpoint(URI(uri)))
+        assertEquals("PONGG", client.exec(ping()).value())
+        client.close()
+        selectorManager.close()
+    }
+}
